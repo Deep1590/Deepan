370aef2ba6aff7de6f9c48d5d666bfa8d2cd72af
PAVILION <PAVILION@HP>
2018-08-04 18:36:33 +0530

    Arithmetic update

---
+++ src/main/java/week1/day1/AccessingArithmeticOperators.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class AccessingArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArithmeticOperations AO=new ArithmeticOperations();
+		AO.addition();
+		AO.multiply();
+		AO.subtract();
+		AO.multiplyNumberOneByTwo();
+		int a;
+		AO.divideNumberTwo( a=2);
+		
+
+	}
+
+}
